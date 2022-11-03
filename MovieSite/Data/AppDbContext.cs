6a730b4d59111589fc39bcd6674f099822693258
Dinip12 <georgi.girhcev@gmail.com>
2022-11-03 17:26:08 +0200

    Add project files.

---
+++ MovieSite/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MovieSite.Models;
+using System.Data.Entity;
+using System.Collections.Generic;
+
+
+namespace MovieSite.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext() : base("Sever=localhost\\sqlexpress;Database=MovieSite;Trusted_Connection=True;")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+            modelBuilder.Entity<Actor_Movie>().HasKey(m=>m.Movie).WithMany()
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MovieSite/Data/Enums/MovieCategory.cs
@@ -0,0 +1,27 @@
+﻿namespace MovieSite.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action,
+        Adventure,
+        Animation,
+        Biography,
+        Comedy,
+        Crime,
+        Drama,
+        Family,
+        Fantasy,
+        FilmNoir,
+        History,
+        Horror,
+        Music,
+        Musical,
+        Mystery,
+        Romance,
+        SciFi,
+        Sport,
+        Thriller,
+        War,
+        Western
+    }
+}
